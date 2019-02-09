b15d7937850213ccbfc60a1b1ee7f30e143f910a
Aziz Ketari <aziz.ketari@yahoo.com>
2019-02-08 18:02:33 -0800

    extract bounding boxes from vision

--- extraction/pdf_to_text.py
+++ extraction/pdf_to_text.py
@@ -4,6 +4,7 @@ import os
 import re
 import sys
 import argparse
+import json
 
 from datetime import timedelta, datetime
 
@@ -71,50 +72,67 @@ def pdf_to_text(bucket_name, batch_size=2):
     bucket = storage_client.get_bucket(bucket_name=bucket_name)
 
     for file_id in pending_file_ids:
+        print(file_id)
+        #try:
+        failed_job = False
+        source_file_path = 'pending-docs/' + file_id + '.pdf'
+        source_blob = bucket.get_blob(source_file_path)
+        stream = io.BytesIO(source_blob.download_as_string())
+        read_pdf = PyPDF2.PdfFileReader(stream)
+        timeout = read_pdf.getNumPages() * 10
 
-        try:
-            failed_job = False
-            source_file_path = 'pending-docs/' + file_id + '.pdf'
-            source_blob = bucket.get_blob(source_file_path)
-            stream = io.BytesIO(source_blob.download_as_string())
-            read_pdf = PyPDF2.PdfFileReader(stream)
-            timeout = read_pdf.getNumPages() * 10
+        logging.info('Time out ' + str(timeout) + ' for file ' + file_id)
 
-            logging.info('Time out ' + str(timeout) + ' for file ' + file_id)
+        # Check if the operation is done
+        operations_dict['{}'.format(file_id)].result(timeout=timeout)
+        blob_list = list(bucket.list_blobs(prefix='vision-results/{}/'.format(file_id)))
+        #print (blob_list)
+        # sort blob list by page numbers
+        #blob_list.sort(key=int(re.match(r"vision-results/.+/output-(\d+)-to-(\d+).json", blob.name).group(1)))
+        text_to_upload = ''
+        bounding_box_to_upload = ''
 
-            # Check if the operation is done
-            operations_dict['{}'.format(file_id)].result(timeout=timeout)
-            blob_list = list(bucket.list_blobs(prefix='vision-results/{}/'.format(file_id)))
-            # sort blob list by page numbers
-            blob_list.sort(key=int(re.match(r"vision-results/.+/output-(\d+)-to-(\d+).json", blob.name).group(1)))
-            text_to_upload = ''
+        document_json = {}
 
-            # Step 3: Extract text from json
-            for blob in blob_list:
-                if len(blob.name.split('/')[-1]) > 1:
-                    json_string = blob.download_as_string()
-                    response = json_format.Parse(
-                        json_string, vision.types.AnnotateFileResponse())
-                    print(response)
-                    for page in response.responses:
-                        annotation = page.full_text_annotation
+        # Step 3: Extract text from json
+        for blob in blob_list:
+            if len(blob.name.split('/')[-1]) > 1:
+                json_string = blob.download_as_string()
+                response = json_format.Parse(
+                    json_string, vision.types.AnnotateFileResponse())
+                #print(response)
+                page_count = 0
+                for page in response.responses:
+                    page_count += 1
+                    annotation = page.full_text_annotation
 
-                    # concatenating all pages in one document
-                    text_to_upload += annotation.text + '\n'
-            # Uploading all pages in one document
-            text_file = bucket.blob('raw-text-files/' + file_id + '.txt')
-            text_file.upload_from_string(text_to_upload)
+                    # create each page content
+                    document_json['page_{}'.format(page_count)]= {}
+                    
+                    # add text of each page 
+                    document_json['page_{}'.format(page_count)]['text']=annotation.text
 
-            # remove file from pending docs to processed docs
-            new_pdf_path = 'processed-docs/' + file_id + '.pdf'
-            bucket.copy_blob(blob=source_blob, destination_bucket=bucket, new_name=new_pdf_path)
-            source_blob.delete()
+                    # add bounding boxes of each page
+                    document_json['page_{}'.format(page_count)]['bounding_boxes']={}
+                    nbr_blocks = len(annotation.pages[0].blocks)
 
-        except:
-            # move pdf to a failed directory
-            new_pdf_path = 'failed-docs/' + file_id + '.pdf'
-            bucket.copy_blob(blob=source_blob, destination_bucket=bucket, new_name=new_pdf_path)
-            source_blob.delete()
+                    for block_idx in range(nbr_blocks):
+                        document_json['page_{}'.format(page_count)]['bounding_boxes']['block_{}'.format(str(block_idx))] = annotation.pages[0].blocks[block_idx].bounding_box
+
+
+        text_file = bucket.blob('cured_vision_output/' + file_id + '.txt')
+        text_file.upload_from_string(str(document_json))
+
+        # remove file from pending docs to processed docs
+        new_pdf_path = 'processed-docs/' + file_id + '.pdf'
+        bucket.copy_blob(blob=source_blob, destination_bucket=bucket, new_name=new_pdf_path)
+        source_blob.delete()
+
+        # except:
+        #     # move pdf to a failed directory
+        #     new_pdf_path = 'failed-docs/' + file_id + '.pdf'
+        #     bucket.copy_blob(blob=source_blob, destination_bucket=bucket, new_name=new_pdf_path)
+        #     source_blob.delete()
 
 
 if __name__ == '__main__':
